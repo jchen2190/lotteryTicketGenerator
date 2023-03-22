781b530a3f3bc3f31a28c2ffaa2dbbdd7885eeab
Jon Chen <jchen2190@gmail.com>
2023-03-21 21:21:55 -0400

    update README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Powerball Lottery Ticket Generator
+
+## General Info
+This project is a lottery ticket generator. User can input how many tickets to generate and random numbers from 1-69 will generate for the first 5th balls (no repeats). The 6th red powerball is a random number from 1-29.
+
+## Deployment
--- script.js
+++ script.js
@@ -1,44 +1,38 @@
 const btn = document.querySelector('button');
 const ticketsDiv = document.getElementById('tickets-div');
 const numBox = document.getElementById('num-box');
-
 btn.addEventListener('click', pickSix)
 
 function pickSix() {
 
     ticketsDiv.innerHTML = "";
-    let numTix = Number(numBox.value); // user input choose # of tickets
+    let numTix = Number(numBox.value); 
 
     for(let t = 0; t < numTix; t++) {
         let sixPicks = [];
-        const pickableNums = []; // holder for lottery numbers 1-69
-        for(let n = 1; n <= 69; n++) {
+        const pickableNums = []; 
+        for(let n = 1; n <= 69; n++) { // 1-69
             pickableNums.push(n);
         }
-        console.log('pickableNums', pickableNums);
 
-        // 5 numbers for white balls
-        for(let t = 0; t < 5; t++) {
+        for(let t = 0; t < 5; t++) { // 5 powerballs
             let r = Math.floor(Math.random() * pickableNums.length);
             let num = pickableNums[r];
             num = num < 10 ? "0" + num : "" + num;
             sixPicks.push(num);
             pickableNums.splice(r,1);
         }
-        sixPicks.sort(); // sort the 5 number balls
+        sixPicks.sort();
 
-        // add the 6th num -- the Powerball number
-        let r = Math.ceil(Math.random() * 26);
+        let r = Math.ceil(Math.random() * 26); // 6th powerball
         r = r < 10 ? "0" + r : "" + r;
         sixPicks.push(r);
 
-        // append the div to hold the lottery pics
         let ticketDiv = document.createElement('div');
         ticketDiv.className = "ticket-div";
         ticketsDiv.appendChild(ticketDiv);
-        ticketDiv.innerHTML = t + 1 + "."; // number each ticket
+        ticketDiv.innerHTML = t + 1 + ".";
 
-        // make 6 ping pong balls as divs -- one per number
         for (let p = 0; p < 6; p++) {
             const pingPongBall = document.createElement('div');
             pingPongBall.className = "ping-pong-ball";
